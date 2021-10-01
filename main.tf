86251dcf552a42d534888a8360c8495266900b0a
parkyes90 <parkyes90@gmail.com>
2021-10-01 13:02:46 +0900

    chore: network

--- main.tf
+++ main.tf
@@ -3,10 +3,39 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-04876f29fd3a5e8ba"
+  ami           = "ami-04876f29fd3a5e8ba"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.example.id]
 
   tags = {
     Name = "terraform-example"
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
+
+resource "aws_security_group" "example" {
+  ingress {
+    from_port = 8080
+    protocol  = "tcp"
+    to_port   = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol  = "tcp"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name: "terraform-example"
+  }
+
 }
